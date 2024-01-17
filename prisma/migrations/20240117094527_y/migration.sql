db1b16c820dce600bc5d688e447a0c14b06fdc33
xppcnn <xppcnn@gmail.com>
2024-01-17 17:45:46 +0800

    update

---
+++ prisma/migrations/20240117094527_y/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `emailVerified` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `user` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `user` DROP COLUMN `createdAt`,
+    DROP COLUMN `emailVerified`,
+    DROP COLUMN `password`,
+    DROP COLUMN `updatedAt`,
+    ADD COLUMN `email_verified` DATETIME(3) NULL;
